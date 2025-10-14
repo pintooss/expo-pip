2f12b33c21e4c8728a52e67d6672cf9175a95c0f
EdgarJMesquita <edgaroboe@outlook.com>
2025-10-14 16:16:13 -0300

    :sparkles: feat: add Picture in Picture support with fragment management

---
+++ android/src/main/java/expo/modules/pip/PictureInPictureHelperFragment.kt
@@ -0,0 +1,25 @@
+package expo.modules.pip
+
+import androidx.fragment.app.Fragment
+
+
+class PictureInPictureHelperFragment: Fragment() {
+    private var listener:PictureInPictureHelperListener? = null
+
+    fun setListener(listener: PictureInPictureHelperListener){
+        this.listener = listener
+    }
+
+    override fun onPictureInPictureModeChanged(isInPictureInPictureMode: Boolean) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode)
+        listener?.onPictureInPictureModeChange(isInPictureInPictureMode)
+    }
+
+    companion object {
+        val id = "${PictureInPictureHelperFragment::class.java.simpleName}"
+    }
+}
+
+interface PictureInPictureHelperListener {
+     fun onPictureInPictureModeChange(isInPictureInPictureMode: Boolean)
+}
